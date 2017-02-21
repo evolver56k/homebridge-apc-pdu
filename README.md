81af32e4f6fde0d2f87b2c10e2e5d3b10dd60c5e
Brady Dreiling <brady@Bradys-Mac-mini.local>
2017-02-21 00:34:37 -0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,35 @@
+# homebridge-apc-pdu
+APC MasterSwitch PDU Plugin for [HomeBridge](https://github.com/nfarina/homebridge) 
+
+# Installation
+1. Install homebridge using `npm install -g homebridge`.
+2. Install this plugin using `npm install -g git+https://github.com/evolver56k/homebridge-apc-pdu.git`.
+3. Update your configuration file. See configuration sample below.
+
+
+# Removal
+1. Stop homebridge.
+2. Remove configuration in `config.json`.
+3. Start homebridge (the plugin will remove cached accessories automatically).
+4. Remove this plugin using `npm remove -g homebridge-apc-pdu`.
+5. Restart homebridge.
+
+# Configuration
+Edit your `config.json` accordingly. Configuration sample:
+```
+	"accessories": [{
+		"accessory": "APC PDU", 
+		"ip": "192.168.0.100", 
+		"name": "APC PDU Switches",
+		"snmp_community": "private"
+	 }]
+```
+
+### Advanced Configuration
+Design based on AP7900 8-Outlet PDU. If you have a PDU with more than 8 outlets, you will need to modify the two
+```for (var i = 1; i < 9; i++) {``` 
+loops to account for the number of outlets your unit has.
+
+
+# Room for improvement
+- Poll unit for number of outlets
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
 
-	homebridge.registerAccessory("homebridge-digipower-pdu", "DigiPower PDU", PDUAccessory);
+	homebridge.registerAccessory("homebridge-apc-pdu", "APC PDU", PDUAccessory);
 }
 
 class PDUAccessory {
@@ -16,22 +16,23 @@ class PDUAccessory {
 	constructor(log, config) {
 		this.log = log;
 		this.services = [];
-		for (var i = 0; i < 8; i++) {
+		for (var i = 1; i < 9; i++) {
 			var service = new Service.Outlet(`Outlet ${i}`, i);
 			this.services.push(service);
 
 			service.getCharacteristic(Characteristic.On)
-				.on('get', this.getOn.bind(this, i))
-				.on('set', this.setOn.bind(this, i));
+				.on("get", this.getOn.bind(this, i ))
+				.on("set", this.setOn.bind(this, i ));
 		}
 
 		this.snmp = snmp.createSession(config.ip, config.snmp_community);
 		this.snmp_get = promisify(this.snmp.get.bind(this.snmp));
+		
 		this.snmp_set = promisify(this.snmp.set.bind(this.snmp));
 
 		var outlet_oids = [];
-		for (var i = 0; i < 8; i++) {
-			outlet_oids.push(`1.3.6.1.4.1.17420.1.2.9.1.14.${i + 1}.0`);
+		for (var i = 1; i < 9; i++) {
+			outlet_oids.push("1.3.6.1.4.1.318.1.1.4.5.2.1.3." + i);
 		}
 		var promises = [];
 		for (var i = 0; i < outlet_oids.length; i += 2) {
@@ -53,7 +54,7 @@ class PDUAccessory {
 					service.displayName = name;
 					service.setCharacteristic(Characteristic.Name, name);
 				}
-				this.log.info('Successfully loaded outlet names: ', names.join(', '));
+				this.log.info("Successfully loaded outlet names: ", names.join(", "));
 			})
 			.catch(error => {
 				this.log.error(error.stack);
@@ -66,11 +67,11 @@ class PDUAccessory {
 
 	getOn(index, callback) {
 		this.log.info(`Retrieving socket ${index}.`);
-		var switch_oid = '1.3.6.1.4.1.17420.1.2.9.1.13.0';
+		var switch_oid = "1.3.6.1.4.1.318.1.1.4.4.2.1.3." + (index) ;
 		this.snmp_get([switch_oid])
 			.then(varbinds => {
-				var switches = varbinds[0].value.toString().split(',');
-				var on = switches[index] == "1"
+				var switches = varbinds[0].value;
+				var on = switches == 1;
 				this.log.info(`Socket ${index} is ${on}.`);
 				callback(null, on);
 			})
@@ -82,16 +83,23 @@ class PDUAccessory {
 
 	setOn(index, on, callback) {
 		this.log.info(`Switching socket ${index} to ${on}.`);
-		var switch_oid = '1.3.6.1.4.1.17420.1.2.9.1.13.0';
+		var switch_oid = "1.3.6.1.4.1.318.1.1.4.4.2.1.3." + (index) ;
 		this.snmp_get([switch_oid])
 			.then(varbinds => {
-				var switches = varbinds[0].value.toString().split(',');
-				switches[index] = on ? '1' : '0';
-				var switch_str = switches.join();
+				var switches = varbinds[0].value;
+				switches = on ? 1 : 0;
+				
+				// APC uses 1=On, 2=Off, so muck boolean logic to work
+				if (switches == 0) {
+					var switch_str = 2;
+				} else  {
+					var switch_str = 1;
+				}
+					
 				varbinds = [
 					{
 						oid: switch_oid,
-						type: snmp.ObjectType.OctetString,
+						type: snmp.ObjectType.Integer,
 						value: switch_str
 					}
 				];
@@ -109,3 +117,4 @@ class PDUAccessory {
 	}
 
 }
+
